b3f0e1095e856dd5dc19a9d295069882bde921f3
oooonduke <wanzerbusi@yahoo.co.jp>
2019-08-12 21:31:25 +0900

    Add files via upload
    センター数学ⅠA平成13年度

---
+++ PythonApplication_suugaku1a_h13.py
@@ -0,0 +1,33 @@
+#センター試験数学ⅠＡ　平成１３年度
+#http://www.dennougumi.co.jp/support/exam/h13/13_1.html
+#Basic言語
+'''
+010 rem 元プログラム
+100 INPUT "X=";X
+110 INPUT "N=";N
+120 Y=1
+140 IF N-2*INT(N/2)=0 THEN GOTO 160
+150 Y=Y*X
+160 N=INT(N/2)
+170 IF N=0 THEN GOTO 190
+175 X=X*X
+180 GOTO 140
+190 PRINT "Y=";Y
+200 END
+'''
+
+#プログラム作者注この問題はpow関数のコーディングを求めている。
+#X ^ N = Y の形式で累乗を求める。
+x = int(input("X= "))
+n = abs(int(input("N= ")))
+y = 1
+
+while True:
+    if(n-2*int(n/2) is not 0):
+        y = int(y) * int(x)
+    n = int(n/2)
+    x = int(x) * int(x)
+    if n == 0:
+        break
+
+print('Y= %d ' % y)
